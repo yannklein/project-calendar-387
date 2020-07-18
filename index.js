59b37fe24305344432f3ad1a5f84198b0f563471
Yann Klein <yann.klein@me.com>
2020-07-18 14:50:46 +0900

    updated the today's card js code

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const today = new Date();
 const cards = document.querySelectorAll(".card-product");
 
 cards.forEach((card) => {
-  const cardDay = card.querySelector("p").innerText.slice(-2);
-  if (today.getDate() === Number.parseInt(cardDay, 10)) {
+  const day = card.querySelector(".card-product-day").innerText;
+  const date = card.querySelector(".card-product-date").innerText;
+  console.log(today.toDateString(), `${day} ${date}`)
+  if (today.toDateString().includes(`${day} ${date}`)) {
     card.classList.add("today");
   }
 });
